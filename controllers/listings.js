c583423d4a52e27902576296433e390b87dd8c23
Gangireddy <pavangangireddy53@gmail.com>
2026-02-15 23:37:31 +0530

    listings images error

--- controllers/listings.js
+++ controllers/listings.js
@@ -25,9 +25,12 @@ module.exports.showListing = async (req, res) => {
 module.exports.createListing = async (req, res) => {
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    if (req.file) {
-        newListing.image = { url: req.file.url, filename: req.file.filename };
+    if (typeof req.file !== "undefined") {
+        let url = req.file.url || req.file.path || req.file.secure_url;
+        let filename = req.file.filename;
+        newListing.image = { url, filename };
     }
+
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
@@ -48,10 +51,13 @@ module.exports.updateListing = async (req, res) => {
     const { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-    if (req.file) {
-        listing.image = { url: req.file.url, filename: req.file.filename };
+    if (typeof req.file !== "undefined") {
+        let url = req.file.url || req.file.path || req.file.secure_url;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
         await listing.save();
     }
+
 
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
